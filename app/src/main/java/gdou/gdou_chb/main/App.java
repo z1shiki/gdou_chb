11ceb5d9045613f6126d19ec43c7449a56e185e2
z1shiki <z1stage@gmail.com>
2016-11-14 19:56:33 +0800

    Initial commit

---
+++ app/src/main/java/gdou/gdou_chb/main/App.java
@@ -0,0 +1,33 @@
+package gdou.gdou_chb.main;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.facebook.stetho.Stetho;
+
+/**
+ * Created by Z1shiki on 2016/11/14.
+ */
+
+public class App extends Application {
+    public static Context mContext;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        super.onCreate();
+        Stetho.initialize(
+                Stetho.newInitializerBuilder(this)
+                        .enableDumpapp(
+                                Stetho.defaultDumperPluginsProvider(this))
+                        .enableWebKitInspector(
+                                Stetho.defaultInspectorModulesProvider(this))
+                        .build());
+
+        mContext = getApplicationContext();
+    }
+
+    public static Context getContext(){
+        return mContext;
+    }
+}
