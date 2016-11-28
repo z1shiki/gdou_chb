25e76d525552bee55abc96935188f290180eaa07
625229636 <13074238968@163.com>
2016-11-28 11:16:08 +0800

    XML for act & ftag

--- app/src/main/java/gdou/gdou_chb/features/user/login/LoginFragment.java
+++ app/src/main/java/gdou/gdou_chb/features/user/login/LoginFragment.java
@@ -7,10 +7,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,6 +25,7 @@ import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.ScrollView;
 import android.widget.TextView;
+
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -33,18 +41,23 @@ import gdou.gdou_chb.util.MVP.BaseFragment;
 
 public class LoginFragment extends BaseFragment implements LoginContract.View {
 
+    @BindView(R.id.toolbar)
+    Toolbar mToolBar;
     @BindView(R.id.login_progress)
     ProgressBar mLoginProgress;
+    @BindView(R.id.login_form)
+    ScrollView mLoginForm;
     @BindView(R.id.account)
     AutoCompleteTextView mAccount;
     @BindView(R.id.password)
     EditText mPassword;
     @BindView(R.id.sign_in_btn)
     Button mSignInBtn;
-    @BindView(R.id.email_login_form)
-    LinearLayout mEmailLoginForm;
-    @BindView(R.id.login_form)
-    ScrollView mLoginForm;
+    @BindView(R.id.sign_up_link)
+    TextView mSignUp;
+    @BindView(R.id.forget_link)
+    TextView mForget;
+
 
     Activity mActivity;
 
@@ -84,6 +97,10 @@ public class LoginFragment extends BaseFragment implements LoginContract.View {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.login_frag, container, false);
 
+        mToolBar = (Toolbar) root.findViewById(R.id.toolbar);
+        setHasOptionsMenu(true);
+        mToolBar.setTitle("");
+        ((AppCompatActivity) getActivity()).setSupportActionBar(mToolBar);
         //setup
         ButterKnife.bind(this, root);
         populateAutoComplete();
