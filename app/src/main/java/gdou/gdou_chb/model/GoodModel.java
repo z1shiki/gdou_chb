70b8fced4f7dec00b72a15ddceb6ee2cd778f7f2
WT <731199365@qq.com>
2016-12-05 11:51:30 +0800

    修改的接口名称

--- app/src/main/java/gdou/gdou_chb/model/GoodModel.java
+++ app/src/main/java/gdou/gdou_chb/model/GoodModel.java
@@ -1,21 +1,19 @@
 package gdou.gdou_chb.model;
 
-
 import com.kymjs.rxvolley.rx.Result;
 
+import gdou.gdou_chb.model.bean.Goods;
 import rx.Observable;
 
 /**
  * Created by WT on 2016/11/30.
  */
-
 public interface GoodModel {
-
     /**
-     * 通过商品ID查询商品
-     * @param goodsId 商品ID
+     * 查询商品
+     * @param goods
      * @return
      */
-    Observable<Result> findByGoodsId(Long goodsId);
+    Observable<Result> findByGoodsId(Goods goods);
 }
 
